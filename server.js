886263c4ce6049cbccdc4089b9cb1508093840eb
mahaubin <mahaubin@gmail.com>
2023-05-05 15:45:41 +0300

    teste de l'API

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
+require('dotenv').config({ path: './config/.env' });
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3030;
+const cors = require('cors');
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+  })
+);
 
 // your code
+app.use('/api', (req, res) => {
+  res.send("L'application demare bien");
+});
 
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT} et cela marche bien`);
+app.listen(5000, () => {
+  console.log(`server started on port 5000 et cela marche bien`);
 });

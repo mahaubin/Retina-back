91f7ce356675d14a77ab38cd007b3b6f97930e45
mahaubin <mahaubin@gmail.com>
2023-05-06 08:47:26 +0300

    ajout de port

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: './config/.env' });
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -16,6 +16,6 @@ app.use('/api', (req, res) => {
   res.send("L'application demare bien");
 });
 
-app.listen(5000, () => {
-  console.log(`server started on port 5000 et cela marche bien`);
+app.listen(8000, () => {
+  console.log(`server started on port 8000 et cela marche bien`);
 });
